59b5b90fc7553ac7b7c17ef96f91b0f070da16ea
gauravs8480 <gaurav.219301591@muj.manipal.edu>
2025-05-07 22:01:42 +0530

    first commit

--- frontend/src/assets/Index.js
+++ frontend/src/assets/Index.js
@@ -1,7 +1,7 @@
 import Logo from "./Logo.png"
 import Bgwall from "./bgwall.jpg"
 import Flag1 from "./GBflag.png";
-import Flag2 from ".SEflag.png";
+import Flag2 from "./sflag.png";
 export {
     Logo,
     Bgwall,
